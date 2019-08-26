56ee438426af08ae221b7d361f49f16c3887454c
tofuadmiral <fuad98ali@gmail.com>
2019-08-26 15:48:02 -0400

    added test extra route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!" # home page routing 
+    # here's some fake data to pretend to render our stuff
+    user = {'username': 'Fuad'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/get_tweets')
+def get_tweets():
+    # just pretend to get new tweets
+    tweets = ['hello', 'tweet 2', 'tweet3']
+    return render_template('get_tweets.html', title='Getting Tweets')
